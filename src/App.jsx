946449260696f3c067d92f5fc964b17019d29c06
Cory Daddona <corydaddona1@hotmail.com>
2023-10-06 18:33:15 -0500

    updated app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Start from "./components/Start"
+import Quiz from "./components/Quiz"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [quiz, setQuiz] = React.useState(false)
+    
+    function startQuiz() { 
+        setQuiz(prev => !prev)
+    }
+    
+    return (
+        <div>
+            <img src="./src/assets/blob-yellow.png" className="blob-yellow" />
+            <img src="./src/assets/blob-blue.png" className="blob-blue" />
+            {quiz ? <Quiz playAgain={startQuiz} /> : <Start handleStart={startQuiz} />}
+        </div>
+    )
 }
-
-export default App
